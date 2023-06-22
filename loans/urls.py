86199e738f87a96bbb6e41e840f72721099319d6
Armando Urquiola Cabrera <patchamama.go@gmail.com>
2023-06-22 13:37:50 +0000

    Add book_detail to show relevant fields of Books and comments

--- loans/urls.py
+++ loans/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.BookList.as_view(), name='home'),
+    path('<slug:slug>/', views.BookDetail.as_view(), name='book_detail'),
 ]
--- loans/views.py
+++ loans/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Book
+
 
 class BookList(generic.ListView):
     model = Book
     queryset = Book.objects.order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 12
+
+
+class BookDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Book.objects
+        book = get_object_or_404(queryset, slug=slug)
+        comments = book.book_comments.filter(approved=True).order_by("-created_on")
+
+        return render(
+            request,
+            "book_detail.html",
+            {
+                "book": book,
+                "comments": comments
+            },
+        )
