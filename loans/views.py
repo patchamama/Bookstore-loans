543a09f3778a6661c5709a64649657209a5eaee4
Armando Urquiola Cabrera <patchamama.go@gmail.com>
2023-06-22 12:31:56 +0000

    Add views and templates to home page

--- loans/views.py
+++ loans/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book
 
-# Create your views here.
+class BookList(generic.ListView):
+    model = Book
+    queryset = Book.objects.order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 12
